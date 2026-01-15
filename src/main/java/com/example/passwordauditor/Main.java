d9ac5db2709228c3956f0fce8bf498ac1246c9d3
Kacper Michalak <michalakkacper2002@gmail.com>
2026-01-15 16:34:48 +0100

    feat: basic visual layout

--- src/main/java/com/example/passwordauditor/Main.java
+++ src/main/java/com/example/passwordauditor/Main.java
@@ -1,6 +1,7 @@
 package com.example.passwordauditor;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,10 +11,15 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Views/MainView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
+        stage.setTitle("Password Auditor");
         stage.setScene(scene);
+        stage.setOnCloseRequest(e ->{
+            Platform.exit();
+            System.exit(0);
+        });
         stage.show();
     }
 }
--- src/main/java/com/example/passwordauditor/controller/MainViewController.java
+++ src/main/java/com/example/passwordauditor/controller/MainViewController.java
@@ -1,11 +1,29 @@
 package com.example.passwordauditor.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 
 public class MainViewController {
 
     @FXML
-    private Button button;
+    private PasswordField passwordField;
+
+    @FXML
+    private Label passwordStrengthLabel;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private HBox analysisHBox;
+    @FXML
+    private VBox analysisVBox;
+
+
+
+
+
 
 }
